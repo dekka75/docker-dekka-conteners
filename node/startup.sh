e38ff7c83c10897481205e6759e03a0c206fb74d
dekka75 <didier.kimes@gmail.com>
2016-10-03 20:50:09 +0200

    First commit

---
+++ node/startup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd /opt/node
+
+# Delete content
+if [ "$DEKKA_FORCE" = "true" ]; then
+    rm -rf *
+fi
+# Install application
+if [ ! -f /opt/node/bin/www ]; then
+    if [ "$DEKKA_APP_NAME" = "express" ]; then
+        tar -xvf /home/express-dekka-app.tar
+    elif [ "$DEKKA_APP_NAME" = "imock-server" ]; then  
+        tar -xvf /home/imock-dekka-serveur.tar
+    fi
+    npm install
+fi
+
+npm start

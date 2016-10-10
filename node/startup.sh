f202257bddf04f877348d8ee3d28c565066aa185
dekka75 <didier.kimes@gmail.com>
2016-10-10 22:28:55 +0200

    Add unit tests

--- node/startup.sh
+++ node/startup.sh
@@ -11,7 +11,7 @@ if [ ! -f /opt/node/bin/www ]; then
     if [ "$DEKKA_APP_NAME" = "express" ]; then
         tar -xvf /home/express-dekka-app.tar
     elif [ "$DEKKA_APP_NAME" = "imock-server" ]; then  
-        tar -xvf /home/imock-dekka-serveur.tar
+        tar -xvf /home/imock-dekka-server.tar
     fi
     npm install
 fi
